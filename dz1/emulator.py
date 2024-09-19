7ed0269425ed6c97a4b2215ab1efd89c827a50be
alndis <148362356+alndis@users.noreply.github.com>
2024-09-19 14:29:39 +0300

    Update emulator.py

--- dz1/emulator.py
+++ dz1/emulator.py
@@ -1,31 +1,16 @@
-import argparse
-import socket
-from vfs import VFS
-from commands import execute_command
 import os
 import configparser
-import tkinter as tk
+import socket
+import threading
+from locale import setlocale
+
+from vfs import VFS
+from commands import execute_command
 
 class TerminalEmulator:
     def __init__(self, vfs, start_script):
-        self.root = tk.Tk()
-        self.root.title("Terminal Emulator")
-        self.root.configure(background='black')  # Set background color to black
-
-        self.text_area = tk.Text(self.root, bg='black',
-                                 fg='green')  # Set text area background to black and text to green
-        self.text_area.pack(fill="both", expand=True)
-
-        self.entry_field = tk.Entry(self.root, bg='black',
-                                    fg='green')  # Set input field background to black and text to green
-        self.entry_field.pack(fill="x")
-
-        self.send_button = tk.Button(self.root, text="Send", command=self.send_command)
-        self.send_button.pack(side=tk.LEFT, padx=10, pady=10)  # Move send button to the left of the input field
-
         self.vfs = vfs
         self.start_script = start_script
-        self.run_script()
 
     def run_script(self):
         if self.start_script:
@@ -33,30 +18,28 @@ class TerminalEmulator:
                 for line in script:
                     self.execute_command(line.strip())
 
-    def send_command(self):
-        command = self.entry_field.get()
-        self.entry_field.delete(0, tk.END)
-        output = self.execute_command(command)  # Call execute_command on self
-        if output:
-            self.text_area.insert(tk.END, output + '\n')
-        else:
-            self.text_area.insert(tk.END, command + '\n')
-
     def execute_command(self, command):
         if command == "exit":
-            self.root.destroy()
+            return False
         else:
-            self.text_area.insert("end", f"{socket.gethostname()}:{self.vfs.current_path}$ {command}\n")
             output = execute_command(self.vfs, command)
             if output.startswith("Error") or output.startswith("cd: no such file or directory:"):
-                self.text_area.insert("end", f"Error: {output}\n", "error")
-                self.text_area.tag_config("error", foreground="red")
+                print(f"Error: {output}")
             else:
-                self.text_area.insert("end", f"{output}\n")
-            self.entry_field.delete(0, tk.END)
+                print(output)
+            return True
 
-    def run(self):
-        self.root.mainloop()
+    def run_terminal(self):
+        os.system("title Virtual Terminal")
+        if self.start_script:
+            self.run_script()
+        while True:
+            command = input(f"{socket.gethostname()}:{self.vfs.current_path}$ ")
+            if not self.execute_command(command):
+                break
+
+    def start(self):
+        threading.Thread(target=self.run_terminal).start()
 
 def main():
     config = configparser.ConfigParser()
@@ -70,7 +53,7 @@ def main():
         os.remove(file_path)
     vfs = VFS(archive_path)
     emulator = TerminalEmulator(vfs, start_script)
-    emulator.run()
+    emulator.start()
 
 if __name__ == "__main__":
     main()
